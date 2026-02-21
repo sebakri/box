afd328d7e93aae3f2f6a1ca212c85300f434e715
Sebastian Krieger <sebastian.krieger85@gmail.com>
2026-02-21 15:51:06 +0100

    fix: resolve Linux compilation errors in sandbox implementation
    - Replace undefined 'syscall.UidMap' and 'syscall.GidMap' with 'syscall.SysProcIDMap'.
    - Disable setgroups for unprivileged User Namespace mappings.

--- cmd/sandbox_linux.go
+++ cmd/sandbox_linux.go
@@ -11,12 +11,13 @@ import (
 func applySandbox(cmd *exec.Cmd, name string, args []string, _ string, _ string) (string, []string) {
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUSER | syscall.CLONE_NEWNS,
-		UidMappings: []syscall.UidMap{
+		UidMappings: []syscall.SysProcIDMap{
 			{ContainerID: 0, HostID: os.Getuid(), Size: 1},
 		},
-		GidMappings: []syscall.GidMap{
+		GidMappings: []syscall.SysProcIDMap{
 			{ContainerID: 0, HostID: os.Getgid(), Size: 1},
 		},
+		GidMappingsEnableSetgroups: false,
 	}
 	return name, args
 }
